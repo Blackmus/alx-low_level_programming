4ec21206a185be45c450cb8afe11ae9c5284c38f
Blackmus <njorogrmoses383@gmail.com>
2023-08-16 05:40:35 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_as_is(char *name)
+{
+printf("Hello, my name is %s\n", name);
+}
+
+/**
+ * print_name_uppercase - print a name in uppercase
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_uppercase(char *name)
+{
+unsigned int i;
+
+printf("Hello, my uppercase name is ");
+i = 0;
+while (name[i])
+{
+if (name[i] >= 'a' && name[i] <= 'z')
+{
+putchar(name[i] + 'A' - 'a');
+}
+else
+{
+putchar(name[i]);
+}
+i++;
+}
+printf("\n");
+}
+
+/**
+ * print_name - print a name using a given printing function
+ * @name: name of the person
+ * @f: pointer to a printing function
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_name("Bob", print_name_as_is);
+print_name("Bob Dylan", print_name_uppercase);
+
+return (0);
+}
